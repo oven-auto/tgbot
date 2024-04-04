ce11c8c185fbda18cf3340f008e75b426b2b3d49
oven-auto <oit@oven-auto.ru>
2024-04-04 16:47:25 +0300

    dump

--- routes/web.php
+++ routes/web.php
@@ -3,11 +3,10 @@
 use App\Http\Controllers\TelegramController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('', [TelegramController::class, 'index'])
-	->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('', [TelegramController::class, 'index']);
 
-Route::get('set', [TelegramController::class, 'set'])
-	->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::get('set', [TelegramController::class, 'set']);
 
-Route::match(['get','post'], 'get', [TelegramController::class, 'get'])
-	->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
+Route::match(['get','post'], 'get', [TelegramController::class, 'get']);
+
+Route::get('info', [TelegramController::class, 'info']);
