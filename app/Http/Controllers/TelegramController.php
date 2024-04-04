0f53eabfd84379dee974ea51600ea9e9d6315c02
oven-auto <oit@oven-auto.ru>
2024-04-04 16:42:35 +0300

    dump

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -8,6 +8,15 @@ use Illuminate\Support\Facades\Http;
 
 class TelegramController extends Controller
 {
+    private $telegramUrl;
+    private $token;
+
+    public function __construct()
+    {
+        $this->telegramUrl =  = 'https://api.telegram.org/bot';
+        $this->token = env('TELEGRAM_BOT_KEY');
+    }
+
     public function index()
     {
         dd('TG');
@@ -17,11 +26,9 @@ class TelegramController extends Controller
 
     public function set()
     {
-        $tgUrl = 'https://api.telegram.org/bot';
-        $token = env('TELEGRAM_BOT_KEY');
         $webUrl = 'https://telegram.oven-auto.ru/get';
 
-        $url = $tgUrl.$token.'/setwebhook?url='.$webUrl;
+        $url = $this->telegramUrl.$this->token.'/setwebhook?url='.$webUrl;
 
         $response = Http::get($url);
 
@@ -41,5 +48,16 @@ class TelegramController extends Controller
         $response = Http::get($url, [
             'message' => $request->all(),
         ]);
+    }
+
+
+
+    public function info()
+    {
+        $url = $this->telegramUrl.$this->token.'/getWebhookInfo';
+
+        $response = Http::get($url);
+
+        dump($response->body());
     }
 }
